a59de2ffcd0e927c4673591b2479ceaf420707fc
jrebeca <timurkalimullin@MacBook-Pro-Timur.local>
2021-03-23 17:34:52 +0500

    message week sc

--- T_bot.py
+++ T_bot.py
@@ -17,23 +17,42 @@ def handle_command(message):
 def start_answer(a):
     if a.data == 'schedule':
         markup_reply = types.InlineKeyboardMarkup()
-        bt_1 = types.InlineKeyboardButton(text='На неделю', callback_data='weekly')
-        bt_2 = types.InlineKeyboardButton(text='На день', callback_data='day')
-        bt_3 = types.InlineKeyboardButton(text='Назад', callback_data='back')
-        markup_reply.add(bt_1, bt_2, bt_3)
+        bt_1 = types.InlineKeyboardButton(text='110-БД', callback_data='110-БД')
+        bt_2 = types.InlineKeyboardButton(text='110-БМ-1', callback_data='110-БМ-1')
+        bt_3 = types.InlineKeyboardButton(text='110-БМ-2', callback_data='110-БМ-2')
+        bt_4 = types.InlineKeyboardButton(text='110-БУ', callback_data='110-БУ')
+        bt_5 = types.InlineKeyboardButton(text='120-БД', callback_data='120-БД')
+        bt_6 = types.InlineKeyboardButton(text='120-БИ', callback_data='120-БИ')
+        bt_7 = types.InlineKeyboardButton(text='120-БУ', callback_data='120-БУ')
+        bt_8 = types.InlineKeyboardButton(text='120-БМО', callback_data='120-БМО')
+        bt_9 = types.InlineKeyboardButton(text='120-БМФ', callback_data='120-БМФ')
+        bt_10 = types.InlineKeyboardButton(text='130-БД', callback_data='130-БД')
+        bt_11 = types.InlineKeyboardButton(text='130-БМ', callback_data='130-БМ')
+        bt_12 = types.InlineKeyboardButton(text='130-БУ', callback_data='130-БУ')
+        bt_13 = types.InlineKeyboardButton(text='140-БД', callback_data='140-БД')
+        bt_14 = types.InlineKeyboardButton(text='140-БМ', callback_data='140-БМ')
+        bt_15 = types.InlineKeyboardButton(text='140-БУ', callback_data='120-БУ')
+        bt_16 = types.InlineKeyboardButton(text='140-ЭБ', callback_data='140-ЭБ')
+        bt_17 = types.InlineKeyboardButton(text='Назад', callback_data='back')
+        bt_18 = types.InlineKeyboardButton(text='Тест', callback_data='test')
+        markup_reply.add(bt_1, bt_2, bt_3,bt_4,bt_5,bt_6,bt_7,bt_8,bt_9,bt_10,bt_11,bt_12,bt_13,bt_14,bt_15,bt_16,bt_17,bt_18)
         bot.send_message(a.message.chat.id, "Выбирай", reply_markup=markup_reply)
     elif a.data == 'cafedrs':
         bot.send_message(a.message.chat.id, 'В нашем вузе имеются следующие кафедры:\n\n[Бухгалтерский учёт, аудит, статистика](http://www.fa.ru/fil/ufa/org/chair/buas/Pages/Home.aspx)\n\n[Математика и информатика](http://www.fa.ru/fil/ufa/org/chair/mi/Pages/Home.aspx)\n\n[Философия, история и право](http://www.fa.ru/fil/ufa/org/chair/fip/Pages/Home.aspx)\n\n[Финансы и кредит](http://www.fa.ru/fil/ufa/org/chair/fik/Pages/Home.aspx)\n\n[Экономика, менеджмент и маркетинг](http://www.fa.ru/fil/ufa/org/chair/emm/Pages/Home.aspx)', parse_mode='Markdown')
     elif a.data == 'help':
         bot.send_message(a.message.chat.id, "*Контакты Уфимского Филиала Финуниверситета:*\n\nТелефон: (347) 251-08-23\n\ne-mail: ufa@fa.ru\n\nАдрес: 450015, г. Уфа, ул. Мустая Карима 69/1", parse_mode='Markdown')
-
     elif a.data == 'back':
         return handle_command(a.message)
+    elif a.data == 'test':
+        main.download_data('130_BD')
+        sc = main.schedule('130_Bd.txt')
+        bot.send_message(a.message.chat.id, "*РАСПИСАНИЕ НА НЕДЕЛЮ*",parse_mode='Markdown')
+        for elem in sc:
+            bot.send_message(a.message.chat.id, elem)
 
-    elif a.data == 'weekly':
-        poop = main.schedule("130_BD.txt")
-        bot.send_message(a.message.chat.id,poop[:])
-    elif a.data == 'day':
-        bot.send_message(a.message.chat.id, "Представь что здесь ваше расписание")
+
+    elif a.data in list(main.data):
+        bot.send_message(a.message.chat.id, main.data[a.data])
+
 
 bot.polling()
--- main.py
+++ main.py
@@ -2,7 +2,27 @@ from subprocess import call
 import requests
 #call(["textutil", "-convert", "txt", "130_BD"])
 
-token = '**'
+token = '*'
+
+data = {
+    '110-БД' : 'http://www.fa.ru/fil/ufa/student/Bak_och/110БД.rtf',
+    '110-БМ-1' : 'http://www.fa.ru/fil/ufa/student/Bak_och/110БМ-1.rtf',
+    '110-БМ-2' : 'http://www.fa.ru/fil/ufa/student/Bak_och/110БМ-2.rtf',
+    '110-БУ': 'http://www.fa.ru/fil/ufa/student/Bak_och/110БУ.rtf',
+    '120-БД': 'http://www.fa.ru/fil/ufa/student/Bak_och/120БД.rtf',
+    '120-БИ': 'http://www.fa.ru/fil/ufa/student/Bak_och/120БИ.rtf',
+    '120-БУ': 'http://www.fa.ru/fil/ufa/student/Bak_och/120БУ.rtf',
+    '120-БМО': 'http://www.fa.ru/fil/ufa/student/Bak_och/120БМО.rtf',
+    '120-БМФ': 'http://www.fa.ru/fil/ufa/student/Bak_och/120БМФ.rtf',
+    '130-БД': 'http://www.fa.ru/fil/ufa/student/Bak_och/130БД.rtf',
+    '130-БM': 'http://www.fa.ru/fil/ufa/student/Bak_och/130БМ.rtf',
+    '130-БУ': 'http://www.fa.ru/fil/ufa/student/Bak_och/130БУ.rtf',
+    '140-БД': 'http://www.fa.ru/fil/ufa/student/Bak_och/140БД.rtf',
+    '140-БМ': 'http://www.fa.ru/fil/ufa/student/Bak_och/140БМ.rtf',
+    '140-БУ': 'http://www.fa.ru/fil/ufa/student/Bak_och/140БУ.rtf',
+    '140-ЭБ': 'http://www.fa.ru/fil/ufa/student/Bak_och/140ЭБ.rtf'
+}
+
 
 url = 'http://www.fa.ru/fil/ufa/student/Bak_och/130БД.rtf'
 
@@ -12,16 +32,14 @@ def download_data(name):
     call(["textutil", "-convert", "txt", str(name)])
 
 def schedule(text):
+    a = []
     a_file = open(text, "r")
     lines = a_file.readlines()
     del lines [:4]
     new_file = open("text_e.txt", "w+")
     for line in lines:
         new_file.write(line)
-        a = []
         a.append(line)
-    return a
+    return(a)
 
-
-#download_data('130_BD')
-#schedule('130_BD.txt')
+print(schedule('130_Bd.txt'))
