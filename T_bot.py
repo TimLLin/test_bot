dda169122d67b0a85521c6d64376bd33fb989fd4
jrebeca <timurkalimullin@MacBook-Pro-Timur.local>
2021-03-23 22:42:06 +0500

    v 1.0

--- T_bot.py
+++ T_bot.py
@@ -34,25 +34,33 @@ def start_answer(a):
         bt_15 = types.InlineKeyboardButton(text='140-БУ', callback_data='120-БУ')
         bt_16 = types.InlineKeyboardButton(text='140-ЭБ', callback_data='140-ЭБ')
         bt_17 = types.InlineKeyboardButton(text='Назад', callback_data='back')
-        bt_18 = types.InlineKeyboardButton(text='Тест', callback_data='test')
-        markup_reply.add(bt_1, bt_2, bt_3,bt_4,bt_5,bt_6,bt_7,bt_8,bt_9,bt_10,bt_11,bt_12,bt_13,bt_14,bt_15,bt_16,bt_17,bt_18)
+        markup_reply.add(bt_1, bt_2, bt_3,bt_4,bt_5,bt_6,bt_7,bt_8,bt_9,bt_10,bt_11,bt_12,bt_13,bt_14,bt_15,bt_16,bt_17)
         bot.send_message(a.message.chat.id, "Выбирай", reply_markup=markup_reply)
+
     elif a.data == 'cafedrs':
-        bot.send_message(a.message.chat.id, 'В нашем вузе имеются следующие кафедры:\n\n[Бухгалтерский учёт, аудит, статистика](http://www.fa.ru/fil/ufa/org/chair/buas/Pages/Home.aspx)\n\n[Математика и информатика](http://www.fa.ru/fil/ufa/org/chair/mi/Pages/Home.aspx)\n\n[Философия, история и право](http://www.fa.ru/fil/ufa/org/chair/fip/Pages/Home.aspx)\n\n[Финансы и кредит](http://www.fa.ru/fil/ufa/org/chair/fik/Pages/Home.aspx)\n\n[Экономика, менеджмент и маркетинг](http://www.fa.ru/fil/ufa/org/chair/emm/Pages/Home.aspx)', parse_mode='Markdown')
+        bot.send_message(a.message.chat.id, 'В нашем вузе имеются следующие кафедры:\n\n[Бухгалтерский учёт, аудит, статистика](http://www.fa.ru/fil/ufa/org/chair/buas/Pages/Home.aspx)\n\n[Математика и информатика](http://www.fa.ru/fil/ufa/org/chair/mi/Pages/Home.aspx)\n\n[Философия, история и право](http://www.fa.ru/fil/ufa/org/chair/fip/Pages/Home.aspx)\n\n[Финансы и кредит](http://www.fa.ru/fil/ufa/org/chair/fik/Pages/Home.aspx)\n\n[Экономика, менеджмент и маркетинг](http://www.fa.ru/fil/ufa/org/chair/emm/Pages/Home.aspx)', parse_mode='Markdown',disable_web_page_preview='true')
     elif a.data == 'help':
         bot.send_message(a.message.chat.id, "*Контакты Уфимского Филиала Финуниверситета:*\n\nТелефон: (347) 251-08-23\n\ne-mail: ufa@fa.ru\n\nАдрес: 450015, г. Уфа, ул. Мустая Карима 69/1", parse_mode='Markdown')
     elif a.data == 'back':
         return handle_command(a.message)
-    elif a.data == 'test':
-        main.download_data('130_BD')
-        sc = main.schedule('130_Bd.txt')
-        bot.send_message(a.message.chat.id, "*РАСПИСАНИЕ НА НЕДЕЛЮ*",parse_mode='Markdown')
-        for elem in sc:
-            bot.send_message(a.message.chat.id, elem)
-
 
     elif a.data in list(main.data):
-        bot.send_message(a.message.chat.id, main.data[a.data])
+        bot.send_message(a.message.chat.id, main.data[a.data],disable_web_page_preview="false")
+        markup_reply2 = types.InlineKeyboardMarkup()
+        bt_1 = types.InlineKeyboardButton(text='Да', callback_data='yes')
+        bt_2 = types.InlineKeyboardButton(text='Нет', callback_data='no')
+        markup_reply2.add(bt_1,bt_2)
+        bot.send_message(a.message.chat.id,"Могу ли я вам еще чем-нибудь помочь?", reply_markup=markup_reply2)
+
+    elif a.data == 'yes':
+        return handle_command(a.message)
+    elif a.data == "no":
+        bot.send_message(a.message.chat.id, "Спасибо за кров мой повелитель")
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() =='старт' or message.text.lower() =='привет' or message.text.lower() =="start":
+        return handle_command(message)
 
 
 bot.polling()
--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-from subprocess import call
-import requests
-#call(["textutil", "-convert", "txt", "130_BD"])
-
-token = '*'
+token = '1739168654:AAGNZoEpFfxUgJtSby3FnRupcOLikkk1Hv8'
 
 data = {
     '110-БД' : 'http://www.fa.ru/fil/ufa/student/Bak_och/110БД.rtf',
@@ -15,31 +11,10 @@ data = {
     '120-БМО': 'http://www.fa.ru/fil/ufa/student/Bak_och/120БМО.rtf',
     '120-БМФ': 'http://www.fa.ru/fil/ufa/student/Bak_och/120БМФ.rtf',
     '130-БД': 'http://www.fa.ru/fil/ufa/student/Bak_och/130БД.rtf',
-    '130-БM': 'http://www.fa.ru/fil/ufa/student/Bak_och/130БМ.rtf',
+    '130-БМ': 'http://www.fa.ru/fil/ufa/student/Bak_och/130БМ.rtf',
     '130-БУ': 'http://www.fa.ru/fil/ufa/student/Bak_och/130БУ.rtf',
     '140-БД': 'http://www.fa.ru/fil/ufa/student/Bak_och/140БД.rtf',
     '140-БМ': 'http://www.fa.ru/fil/ufa/student/Bak_och/140БМ.rtf',
     '140-БУ': 'http://www.fa.ru/fil/ufa/student/Bak_och/140БУ.rtf',
     '140-ЭБ': 'http://www.fa.ru/fil/ufa/student/Bak_och/140ЭБ.rtf'
 }
-
-
-url = 'http://www.fa.ru/fil/ufa/student/Bak_och/130БД.rtf'
-
-def download_data(name):
-    r = requests.get(url, allow_redirects=True)
-    open(name,"wb").write(r.content)
-    call(["textutil", "-convert", "txt", str(name)])
-
-def schedule(text):
-    a = []
-    a_file = open(text, "r")
-    lines = a_file.readlines()
-    del lines [:4]
-    new_file = open("text_e.txt", "w+")
-    for line in lines:
-        new_file.write(line)
-        a.append(line)
-    return(a)
-
-print(schedule('130_Bd.txt'))
