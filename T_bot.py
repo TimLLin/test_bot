a233fe7265ebe36add5edf2043698ef42e88d4a2
jrebeca <timurkalimullin@MacBook-Pro-Timur.local>
2021-03-26 19:26:35 +0500

    v 1.2.3

--- T_bot.py
+++ T_bot.py
@@ -2,7 +2,7 @@ import telebot
 import main
 from telebot import types
 
-bot = telebot.TeleBot('1739168654:AAGNZoEpFfxUgJtSby3FnRupcOLikkk1Hv8')
+bot = telebot.TeleBot('**')
 
 
 @bot.message_handler(commands=['start'])
@@ -11,8 +11,7 @@ def handle_command(message):
     bt_1 = types.InlineKeyboardButton(text='Расписание', callback_data='schedule')
     bt_2 = types.InlineKeyboardButton(text='Кафедры', callback_data='cafedrs')
     bt_3 = types.InlineKeyboardButton(text='Помощь', callback_data='help')
-    bt_4 = types.InlineKeyboardButton(text='Credits', callback_data='creators')
-    markup_inline.add(bt_1, bt_2, bt_3, bt_4)
+    markup_inline.add(bt_1, bt_2, bt_3)
     bot.send_message(message.chat.id, 'Что тебе показать?', reply_markup=markup_inline)
 
 
@@ -20,26 +19,25 @@ def handle_command(message):
 def start_answer(a):
     if a.data == 'schedule':
         markup_reply = types.InlineKeyboardMarkup()
-        bt_1 = types.InlineKeyboardButton(text='110-БД', callback_data='110-БД')
-        bt_2 = types.InlineKeyboardButton(text='110-БМ-1', callback_data='110-БМ-1')
-        bt_3 = types.InlineKeyboardButton(text='110-БМ-2', callback_data='110-БМ-2')
-        bt_4 = types.InlineKeyboardButton(text='110-БУ', callback_data='110-БУ')
-        bt_5 = types.InlineKeyboardButton(text='120-БД', callback_data='120-БД')
-        bt_6 = types.InlineKeyboardButton(text='120-БИ', callback_data='120-БИ')
-        bt_7 = types.InlineKeyboardButton(text='120-БУ', callback_data='120-БУ')
-        bt_8 = types.InlineKeyboardButton(text='120-БМО', callback_data='120-БМО')
-        bt_9 = types.InlineKeyboardButton(text='120-БМФ', callback_data='120-БМФ')
-        bt_10 = types.InlineKeyboardButton(text='130-БД', callback_data='130-БД')
-        bt_11 = types.InlineKeyboardButton(text='130-БМ', callback_data='130-БМ')
-        bt_12 = types.InlineKeyboardButton(text='130-БУ', callback_data='130-БУ')
-        bt_13 = types.InlineKeyboardButton(text='140-БД', callback_data='140-БД')
-        bt_14 = types.InlineKeyboardButton(text='140-БМ', callback_data='140-БМ')
-        bt_15 = types.InlineKeyboardButton(text='140-БУ', callback_data='140-БУ')
+        bt_1 = types.InlineKeyboardButton(text='110-БД', callback_data='110-БД.docx')
+        bt_2 = types.InlineKeyboardButton(text='110-БМ-1', callback_data='110-БМ-1.docx')
+        bt_3 = types.InlineKeyboardButton(text='110-БМ-2', callback_data='110-БМ-2.docx')
+        bt_4 = types.InlineKeyboardButton(text='110-БУ', callback_data='110-БУ.docx')
+        bt_5 = types.InlineKeyboardButton(text='120-БД', callback_data='120-БД.docx')
+        bt_6 = types.InlineKeyboardButton(text='120-БИ', callback_data='120-БИ.docx')
+        bt_7 = types.InlineKeyboardButton(text='120-БУ', callback_data='120-БУ.docx')
+        bt_8 = types.InlineKeyboardButton(text='120-БМО', callback_data='120-БМО.docx')
+        bt_9 = types.InlineKeyboardButton(text='120-БМФ', callback_data='120-БМФ.docx')
+        bt_10 = types.InlineKeyboardButton(text='130-БД', callback_data='130-БД.docx')
+        bt_11 = types.InlineKeyboardButton(text='130-БМ', callback_data='130-БМ.docx')
+        bt_12 = types.InlineKeyboardButton(text='130-БУ', callback_data='130-БУ.docx')
+        bt_13 = types.InlineKeyboardButton(text='140-БД', callback_data='140-БД.docx')
+        bt_14 = types.InlineKeyboardButton(text='140-БМ', callback_data='140-БМ.docx')
+        bt_15 = types.InlineKeyboardButton(text='140-БУ', callback_data='140-БУ.docx')
         bt_16 = types.InlineKeyboardButton(text='140-ЭБ', callback_data='140-ЭБ.docx')
         bt_17 = types.InlineKeyboardButton(text='Назад', callback_data='back')
-        bt_18 = types.InlineKeyboardButton(text='Test', callback_data='test')
-        markup_reply.add(bt_1, bt_2, bt_3, bt_4, bt_5, bt_6, bt_7, bt_8, bt_9, bt_10, bt_11, bt_12, bt_13, bt_14, bt_15,
-                         bt_16, bt_17,bt_18)
+        bt_18 = types.InlineKeyboardButton(text='тест', callback_data='test')
+        markup_reply.add(bt_1, bt_2, bt_3, bt_4, bt_5, bt_6, bt_7, bt_8, bt_9, bt_10, bt_11, bt_12, bt_17, bt_18)
         bot.send_message(a.message.chat.id, "Выбирай", reply_markup=markup_reply)
 
     elif a.data == 'cafedrs':
@@ -62,7 +60,8 @@ def start_answer(a):
         return handle_command(a.message)
 
     elif a.data in list(main.data):
-        bot.send_message(a.message.chat.id,main.data[a.data])
+        schedule = main.download_data(a.data,main.data[a.data])
+        bot.send_document(a.message.chat.id,schedule)
         markup_reply4 = types.InlineKeyboardMarkup()
         bt_3 = types.InlineKeyboardButton(text='Да', callback_data='yes')
         bt_4 = types.InlineKeyboardButton(text='Нет', callback_data='no')
@@ -76,25 +75,12 @@ def start_answer(a):
         bt_5 = types.InlineKeyboardButton(text='Тык', callback_data='yes')
         markup_reply5.add(bt_5)
         bot.send_message(a.message.chat.id, "Как только понадоблюсь - тыкни!", reply_markup=markup_reply5)
-    elif a.data == 'creators':
-        markup_reply6 = types.InlineKeyboardMarkup()
-        bt_6 = types.InlineKeyboardButton(text='Назад', callback_data='yes')
-        markup_reply6.add(bt_6)
-        bot.send_message(a.message.chat.id,
-                         'Код написали:\nЖуравлёв Алексей и Калимуллин Тимур\n\nНаучный руководитель:\nФархиева Светлана Анатольевна',
-                         reply_markup=markup_reply6)
 
-    elif a.data == 'test':
-       f=open('140-ЭБ.docx','rb')
-       bot.send_document(a.message.chat.id,f)
+
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     if message.text.lower() == 'старт' or message.text.lower() == 'привет' or message.text.lower() == "start":
         return handle_command(message)
-    if message.text.lower() == 'гачимучи' or message.text.lower() == 'гачи':
-        bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAECGMNgWszxYKh0CWKG4IWj5bO77ARJAgAC2wADZj9IJlhsnQls0emgHgQ')
-        bot.send_message(message.chat.id, '♂Oh, yes sir♂')
-
 
 bot.polling()
--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 import requests
 
 data = {
-    '110-БД' : 'http://www.fa.ru/fil/ufa/student/Bak_och/110БД.rtf',
-    '110-БМ-1' : 'http://www.fa.ru/fil/ufa/student/Bak_och/110БМ-1.rtf',
-    '110-БМ-2' : 'http://www.fa.ru/fil/ufa/student/Bak_och/110БМ-2.rtf',
-    '110-БУ': 'http://www.fa.ru/fil/ufa/student/Bak_och/110БУ.rtf',
-    '120-БД': 'http://www.fa.ru/fil/ufa/student/Bak_och/120БД.rtf',
-    '120-БИ': 'http://www.fa.ru/fil/ufa/student/Bak_och/120БИ.rtf',
-    '120-БУ': 'http://www.fa.ru/fil/ufa/student/Bak_och/120БУ.rtf',
-    '120-БМО': 'http://www.fa.ru/fil/ufa/student/Bak_och/120БМО.rtf',
-    '120-БМФ': 'http://www.fa.ru/fil/ufa/student/Bak_och/120БМФ.rtf',
-    '130-БД': 'http://www.fa.ru/fil/ufa/student/Bak_och/130БД.rtf',
-    '130-БМ': 'http://www.fa.ru/fil/ufa/student/Bak_och/130БМ.rtf',
-    '130-БУ': 'http://www.fa.ru/fil/ufa/student/Bak_och/130БУ.rtf',
-    '140-БД': 'http://www.fa.ru/fil/ufa/student/Bak_och/140БД.rtf',
-    '140-БМ': 'http://www.fa.ru/fil/ufa/student/Bak_och/140БМ.rtf',
-    '140-БУ': 'http://www.fa.ru/fil/ufa/student/Bak_och/140БУ.rtf',
-    '140-ЭБ.docx': 'http://www.fa.ru/fil/ufa/student/Bak_och/График%20пересдач.docx'
+    '110-БД.docx' : 'http://www.fa.ru/fil/ufa/student/Bak_och/110БД.docx',
+    '110-БМ-1.docx' : 'http://www.fa.ru/fil/ufa/student/Bak_och/110БМ-1.docx',
+    '110-БМ-2.docx' : 'http://www.fa.ru/fil/ufa/student/Bak_och/110БМ-2.docx',
+    '110-БУ.docx': 'http://www.fa.ru/fil/ufa/student/Bak_och/110БУ.docx',
+    '120-БД.docx': 'http://www.fa.ru/fil/ufa/student/Bak_och/120БД.docx',
+    '120-БИ.docx': 'http://www.fa.ru/fil/ufa/student/Bak_och/120БИ.docx',
+    '120-БУ.docx': 'http://www.fa.ru/fil/ufa/student/Bak_och/120БУ.docx',
+    '120-БМО.docx': 'http://www.fa.ru/fil/ufa/student/Bak_och/120БМО.docx',
+    '120-БМФ.docx': 'http://www.fa.ru/fil/ufa/student/Bak_och/120БМФ.docx',
+    '130-БД.docx': 'http://www.fa.ru/fil/ufa/student/Bak_och/130БД.docx',
+    '130-БМ.docx': 'http://www.fa.ru/fil/ufa/student/Bak_och/130БМ.docx',
+    '130-БУ.docx': 'http://www.fa.ru/fil/ufa/student/Bak_och/130БУ.docx',
+    '140-БД.docx': 'http://www.fa.ru/fil/ufa/student/Bak_och/140БД.docx',
+    '140-БМ.docx': 'http://www.fa.ru/fil/ufa/student/Bak_och/140БМ.docx',
+    '140-БУ.docx': 'http://www.fa.ru/fil/ufa/student/Bak_och/140БУ.docx',
+    '140-ЭБ.docx': 'http://www.fa.ru/fil/ufa/student/Bak_och/140ЭБ.docx'
 }
 
 
 def download_data(name,url):
     r = requests.get(url, allow_redirects=True)
     open(name, "wb").write(r.content)
+    file = open(name, 'rb')
+    return file
 
 
-download_data('130БД.doc','http://www.fa.ru/fil/ufa/student/Bak_och/130БД.doc')
+
+download_data('130-БД.docx','http://www.fa.ru/fil/ufa/student/Bak_och/130БД.docx')
